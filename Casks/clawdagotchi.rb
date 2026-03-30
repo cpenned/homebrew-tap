d6c1a11c561d09765ec026f165d4381dc3027ba1
Chris Pennington <cpenn368@gmail.com>
2026-03-30 15:21:33 -0600

    chore: bump clawdagotchi to v1.6.1

--- Casks/clawdagotchi.rb
+++ Casks/clawdagotchi.rb
@@ -1,6 +1,6 @@
 cask "clawdagotchi" do
-  version "1.6.0"
-  sha256 "2e532e9321e3399fb89f5754753d22ec4ba05e953538eed8ca0736ddc4e05df7"
+  version "1.6.1"
+  sha256 "540124f85fd2dedd9c29263f56ea7d64718d69ca3ddc6dd08caedd65a0b895ce"
 
   url "https://github.com/cpenned/clawdagotchi/releases/download/v#{version}/Clawdagotchi-#{version}.dmg"
   name "Clawdagotchi"

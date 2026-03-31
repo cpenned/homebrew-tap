7de22f0b60348753c4e669e7a33a0d559954edcf
cpenned <66193019+cpenned@users.noreply.github.com>
2026-03-30 19:31:57 -0600

    chore: bump clawdagotchi to v1.7.1

--- Casks/clawdagotchi.rb
+++ Casks/clawdagotchi.rb
@@ -1,6 +1,6 @@
 cask "clawdagotchi" do
-  version "1.7.0"
-  sha256 "bc5cf781eac56231fd5c69bc45cd698e0968ef6075c3c1301abf1fc6c81354a8"
+  version "1.7.1"
+  sha256 "b7b577e4ece6c6fc4102bdb0af8711a95f1a8213a8357aaa588ddc30271f8c71"
 
   url "https://github.com/cpenned/clawdagotchi/releases/download/v#{version}/Clawdagotchi-#{version}.dmg"
   name "Clawdagotchi"
